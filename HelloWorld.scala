f692cf879e52211ae18efbe5ebc325c8a7e0bdf1
Tobi <b1t0@icloud.com>
2016-10-11 11:25:08 +0200

    Create HelloWorld.scala

---
+++ HelloWorld.scala
@@ -0,0 +1,10 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
+
+// Compile this:
+// $ scalac HelloWorld.scala
+// Run it :
+// $ scala HelloWorld
