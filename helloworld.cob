f57c6a43f6b3828bc3fe2555990d0e56dc5315ca
Michael Hoogkamer <michael.hoogkamer@gmail.com>
2016-10-11 17:47:59 +0300

    Hello World in Cobol

---
+++ helloworld.cob
@@ -0,0 +1,7 @@
+      * Code needs to be compiled for anything to show on display.
+      * Compiles on Linux with "cobc -x -o hw.exe helloworld.cob"
+000100 IDENTIFICATION DIVISION.
+000200 PROGRAM-ID. HELLOWORLD.
+000300 PROCEDURE DIVISION.
+000400    DISPLAY 'Hello world!'.
+000500    STOP RUN.
