5cab97105b804d92dfeb84069563c4b22ccdb7d3
Codeship dcoy <david@obsidianspork.io>
2016-10-12 13:17:11 -0500

    add semicolon

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-
-int main() {
-   /* my first program in C */
-   printf("Hello World!\n");
-   
-   return 0;
+#include<stdio.h>
+int main()
+{
+	printf("Hello world\n");
+	return 0;
 }
