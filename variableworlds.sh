09534ea6ecd85b17590e68df6c93a5d2e857cdcd
DJ Valenzuela <djdavidjustin@gmail.com>
2016-10-12 15:44:29 -0500

    variable script

---
+++ variableworlds.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# some random stuff
+
+clear
+echo "hacktheworld!!!!"
+sleep 1 
+echo "How is that hack coming?" 
+sleep 1 
+read aww
+
+echo " $aww, Really!?" 
+echo "$aww" >> Crybaby.txt
