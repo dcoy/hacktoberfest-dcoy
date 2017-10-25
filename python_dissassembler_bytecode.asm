85425ef3b4abb49754913672fb9396dd4374ee3b
G POORNA PRUDHVI <poornagurram@users.noreply.github.com>
2017-10-26 01:42:15 +0530

    Python Assembly code
    Disassembled python byte code for hello world

---
+++ python_dissassembler_bytecode.asm
@@ -0,0 +1,9 @@
+#def hello():
+#print('Hello World!!')
+
+0 LOAD_GLOBAL              0 (print)
+2 LOAD_CONST               1 ('Hello World!!')
+4 CALL_FUNCTION            1
+6 POP_TOP
+8 LOAD_CONST               0 (None)
+10 RETURN_VALUE
